af7f4497ace3e0fecfa5b63036aa7a151264489f
Palash Singh Raghuwanshi <palash@sbnri.com>
2025-08-23 02:19:12 +0530

    Initial project setup for Railway deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sbnri_hackathon.wsgi --log-file -
---
+++ dtaa/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DtaaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dtaa"
